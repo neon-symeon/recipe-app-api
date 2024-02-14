06b05b772f777b3aa0095cf4fe53c9e2638c0e61
neon-symeon <szymon.jan.marek@gmail.com>
2024-02-14 15:20:06 +0100

    Tags addings in progress. Searching for bugs.

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,85 @@
+"""
+Tests for the Tag API.
+"""
+from typing import Any
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a user fo tests."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+
+class PublicTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+    def setUp(self):
+        self.client = APIClient()
+        # And this test case basically means an unauthenticated request
+        # to the tags-list endpoint. And what we would expect in our situation
+        # is to just get a 401 unauthorized. We don't expect to see
+        # any tags because we haven't authenticated, so it doesn't know which
+        # user to send the tags for. So this is the thing we're testing here is
+        # just that the API requires authentication.
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """Test the authorized user tags API."""
+    def setUp(self):
+        """That's just the setup here. So we create a user, we create a client
+        and then we authenticate the user on the clients or request are made
+        to the client, are authenticated as a user that we created."""
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        # So this is just two sample tags that we're going to use to test with.
+        # That's the setup that we need to do.
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+
+        # Now we're actually execute the code by calling the API.
+        res = self.client.get(TAGS_URL)
+
+        # And then we're going to assert that the response is what we expect.
+        # We will check the results.
+        tags = Tag.objects.all().order_by('-name')
+        # And then we're going to serialize the tags and then compare the
+        # response data to the serialized data. So it will be a list of objects.
+        serializer = TagSerializer(tags, many=True)
+        # And then we're going to compare the response data to the serialized data.
+        self.assertEqual(res.data, status.HTTP_200_OK)
+        # And then we're going to compare the response data to the serialized data.
+        self.assertEqual(res.data, serializer.data)
+        # So that's testing the retrieve tags, basic test.
+
+    def test_tags_limited_to_user(self):
+        """Test list of tags is limited to authenticated user."""
+        # Here we've created another user.
+        user2 = create_user(email='user2@example.com')
+        # And then we've created a tag for that particular user.
+        # It's going to be a fruity tag :)
+        Tag.objects.create(user=user2, name='Fruity')
+        # And then we're going to create another tag for our authenticated user.
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,18 @@
 """
 Views for the recipe APIs.
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,  # Mixins is just things that you can mix into a view
+    # to add additional functionality.
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 from recipe import serializers
 
 
@@ -29,4 +36,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         """Create a new recipe."""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]  ## It is very important that
+    # this is spelled correctly. So if you misspell any of these, then you'll
+    # have unpredictable behavior. It won't necessarily give you an error,
+    # but it will give you a lot of issues that are very hard to debug.
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user. We are overriding the
+        get_queryset function to filter the tags by the user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag."""
         serializer.save(user=self.request.user)
