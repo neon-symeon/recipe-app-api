ac808e94157b8eefefd32675f46c8a462898420a
neon-symeon <szymon.jan.marek@gmail.com>
2024-02-18 12:57:45 +0100

    Added: Tags, Ingredients, Images and Filtering Implemented.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,7 +6,17 @@ from rest_framework import serializers
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredients."""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -22,30 +32,18 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
     tags = TagSerializer(many=True, required=False)  # many=True this is
     # going to be a list of items.
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'link', 'tags',
+            'ingredients',
+        ]
         read_only_fields = ['id']
 
-    # These nested serializers, those are read only. So that means you can read
-    # he values, but you can't create items with those values. We want to add
-    # the feature to be able to create them. We're going to do that by adding
-    # a new method to the class that allows us to override the behavior of
-    # the create functionality.
-    def create(self, validated_data):
-        """Create a recipe."""
-        # So this is why we are using pop here because we want to make sure we
-        # remove the tags before we create the recipe using this here. Because
-        # if you were to just pass in tags directly to the recipe model, that's
-        # not going to work because it's a recipe model and expects only the
-        # values for the recipe and it expects tags to be assigned as a related
-        # field, so expects it to be created separately and added as a
-        # relationship to recipe from the many. Too many field that we added.
-        tags = validated_data.pop('tags', [])
-        # With the rest of the validated data - everything excluding tags -
-        # we're going to create a new recipe with `**validated_data` values.
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
         # Then we're getting the authenticate user. Because we're doing this in
         # a sterilizer and not the view, we need to use this self.context
         # request.
@@ -59,12 +57,89 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag,
             )
             recipe.tags.add(tag_obj)
+    # These nested serializers, those are read only. So that means you can read
+    # he values, but you can't create items with those values. We want to add
+    # the feature to be able to create them. We're going to do that by adding
+    # a new method to the class that allows us to override the behavior of
+    # the create functionality.
+
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        # So the idea is that it's going to either get an existing ingredient
+        # or create a new ingredient. Ultimately, it's going to be adding an
+        # ingredient to the recipe object. So we start by retrieving the
+        # authenticated user so we can assign the ingredient to the correct
+        # user. Then we're looping through all of the ingredients that were
+        # passed in as an argument here at the top. And then we are calling
+        # the get_or_create helper method on the objects manager for our
+        # ingredient model.
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, _ = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        # So this is why we are using pop here because we want to make sure we
+        # remove the tags before we create the recipe using this here. Because
+        # if you were to just pass in tags directly to the recipe model, that's
+        # not going to work because it's a recipe model and expects only the
+        # values for the recipe and it expects tags to be assigned as a related
+        # field, so expects it to be created separately and added as a
+        # relationship to recipe from the many. Too many field that we added.
+        tags = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
+        # With the rest of the validated data - everything excluding tags -
+        # we're going to create a new recipe with `**validated_data` values.
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+        self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
+
+    def update(self, instance, validated_data):  # instance is the existing
+        # object that we're going to update with the validated data.
+        """Update a recipe."""
+        # We're going to pop the tags off the validated data. We're going to
+        # assign the tags to a variable. We're going to use the get method to
+        # get the tags from the validated data. If it doesn't exist, we're
+        # going to default to an None value.
+        tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+        # We're going to call the parent class update method. This is going to
+        # update the instance with the validated data. So it's going to update
+        # the title, time_minutes, price, link, and any other fields that we
+        # pass in.
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,21 +1,49 @@
 """
 Views for the recipe APIs.
 """
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
 from rest_framework import (
     viewsets,
     mixins,  # Mixins is just things that you can mix into a view
     # to add additional functionality.
+    status,
 )
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
     Recipe,
     Tag,
+    Ingredient
 )
 from recipe import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='tags',
+                type=OpenApiTypes.STR,
+                description='Comma separated list of tags to filter',
+            ),
+            OpenApiParameter(
+                name='ingredients',
+                type=OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
@@ -23,14 +51,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers."""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrive recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+        # We're retrieving the query params called tags and ingredients.
+        # This is a comma separated list provided by the user as a string.
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-id').distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
@@ -38,14 +87,44 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data,
+        )
 
-class TagViewSet(mixins.DestroyModelMixin,  # Allows to delete a model.
-                 mixins.UpdateModelMixin,  # Allows to update a model.
-                 mixins.ListModelMixin,
-                 viewsets.GenericViewSet):
-    """Manage tags in the database."""
-    serializer_class = serializers.TagSerializer
-    queryset = Tag.objects.all()
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            )
+        ]
+    )
+)
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,  # Allows to delete model
+                            mixins.UpdateModelMixin,  # Allows to update amodel
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base viewset for user owned recipe attributes."""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]  # It is very important that
     # this is spelled correctly. So if you misspell any of these, then you'll
@@ -53,10 +132,29 @@ class TagViewSet(mixins.DestroyModelMixin,  # Allows to delete a model.
     # but it will give you a lot of issues that are very hard to debug.
 
     def get_queryset(self):
-        """Filter queryset to authenticated user. We are overriding the
-        get_queryset function to filter the tags by the user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        """Filter queryset to authenticated user."""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
 
-    def perform_create(self, serializer):
-        """Create a new tag."""
-        serializer.save(user=self.request.user)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    # def perform_create(self, serializer):
+    #     """Create a new tag."""
+    #     serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
